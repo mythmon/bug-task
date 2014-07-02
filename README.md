59366d402eee162284096f64a2c27912106e9580
Mike Cooper <mythmon@gmail.com>
2014-07-02 16:36:42 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Add tasks in [Taskwarrior](http://taskwarrior.org) that correspond to Bugzilla bugs.
---
+++ index.js
@@ -0,0 +1,35 @@
+var PromiseProxy = require('proxied-promise-object');
+var Promise = require('es6-promises');
+var bz = require('bz');
+var nomnom = require('nomnom');
+
+var tw = require('./lib/taskwarrior');
+var utils = require('./lib/utils');
+
+var opts = nomnom.parse();
+var bugzilla = PromiseProxy(Promise, bz.createClient());
+var error = false;
+
+Promise.all(opts._.map(function(bugNo) {
+  return bugzilla.getBug(bugNo)
+  .then(function(bug) {
+    return tw.addTask({
+      description: bug.summary + ' [Bug ' + bugNo + ']',
+    });
+  })
+  .then(function() {
+    console.log(bugNo, '✓');
+  })
+  .catch(function(err) {
+    console.log(bugNo, '✗');
+    console.error(err);
+    error = true;
+  });
+}))
+.then(function() {
+  process.exit(error);
+})
+.catch(function(err) {
+  console.log('Error', error);
+  process.exit(1);
+});
