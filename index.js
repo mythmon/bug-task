a4c759ffa561bc750c37925726f6f8933a3741aa
Mike Cooper <mythmon@gmail.com>
2014-07-02 17:31:21 -0700

    Better output.

--- index.js
+++ index.js
@@ -7,28 +7,33 @@ var nomnom = require('nomnom');
 var tw = require('./lib/taskwarrior');
 var utils = require('./lib/utils');
 
-var opts = nomnom.parse();
+var opts = nomnom
+  .script('bugtask')
+  .options({
+    bugs: {
+      position: 0,
+      help: 'Bug numbers to add tasks for.',
+      list: true,
+      required: true,
+    },
+  })
+  .parse();
+
 var bugzilla = PromiseProxy(Promise, bz.createClient());
 var error = false;
 
-if (opts._.length === 0) {
-  console.log('Usage:', process.argv[0], 'BUG [BUG ...]');
-  process.exit(1);
-}
-
-Promise.all(opts._.map(function(bugNo) {
+Promise.all(opts.bugs.map(function(bugNo) {
   return bugzilla.getBug(bugNo)
   .then(function(bug) {
     return tw.addTask({
       description: bug.summary + ' [Bug ' + bugNo + ']',
     });
   })
-  .then(function() {
-    console.log(bugNo, '✓');
+  .then(function(task) {
+    console.log('Bug ' + bugNo + ': Added task ' + task.id);
   })
   .catch(function(err) {
-    console.log(bugNo, '✗');
-    console.error(err);
+    console.log('Bug ' + bugNo + ' ', err.message);
     error = true;
   });
 }))
